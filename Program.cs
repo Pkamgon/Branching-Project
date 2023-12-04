6dbcf7604f25cceb771d77efaff1c59fb4c0e887
Pkamgon <Pkamgon@gmail.com>
2023-12-04 16:28:38 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Branching_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("Please enter dimensionSize");
+            int dimensionSize = Convert.ToInt32(Console.ReadLine());
+            string result = dimensionSize > 50 ? "You have the right weight" : "Package too heavy to be shipped via Package Express. Have a good day";
+            Console.WriteLine(result);
+            int packageWeight = 40;
+            int packageWidth= 10;
+            int packageHeight= 12;
+            int packageLength = 11;
+            Console.WriteLine((packageLength * packageWidth * packageHeight) * packageWeight);
+            Console.WriteLine("Please enter packageWidth");
+            int width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter packageHeight");
+            int height = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the packagelength");
+            int length = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your estimatedTotal for shipping this package is");
+            int estimatedTotal = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(estimatedTotal);
+            Console.ReadLine();
+
+        }
+    }
+}
